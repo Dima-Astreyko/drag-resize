60242045076efe73c7e9a38a751e6a5de2190735
Dima Astreyko <d.astreyko@itransition.com>
2024-02-05 19:42:49 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ResizableComponent } from "./resisable/resizable.component";
+import { CdkDrag } from "@angular/cdk/drag-drop";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ResizableComponent, CdkDrag],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'untitled';
+
+  startRect = { x: 150, y: 400, width: 100, height: 100 };
+
 }
